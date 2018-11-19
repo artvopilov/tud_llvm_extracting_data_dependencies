16bdeca25a0b4740b76c2b8b5d52a281879b390e
PDH <44472690+PascalDH@users.noreply.github.com>
2018-11-19 18:28:30 +0100

    use-def pass with basic functionality

--- usedef2.cpp
+++ usedef2.cpp
@@ -2,12 +2,13 @@ bool runOnFunction (Function &F) override{
 	for (Function::iterator bb = F.begin(), e = F.end(); bb != e; bb++) {
 		for (BasicBlock::iterator i = bb->begin(), ie = bb->end(); i != ie; i++) {
 			Instruction* inst = dyn_cast<Instruction>(i);
-			if (inst->getOpcode() == Instruction::Add) {
+			// if (inst->getOpcode() == Instruction::Add) {
 				for (Use &U: inst->operands()) {
-					value *v = U.get();
+					Value *v = U.get();
 					errs() << *v >> "\n";
 				}
-			}
+			// }
 		}
 	}
+	return false;
 }
